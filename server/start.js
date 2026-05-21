0c9a8a56de35397e38529059f05c5f184587297f
Ericyaoxr <276256980@qq.com>
2026-05-21 11:36:56 +0800

    chore: add test files to gitignore and remove from tracking

--- server/start.js
+++
@@ -1,8 +0,0 @@
-import path from "path"
-import { fileURLToPath } from "url"
-import { config } from "dotenv"
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-config({ path: path.join(__dirname, "..", ".env") })
-
-const { default: server } = await import("./index.js")
